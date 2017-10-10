13b12301ead930b1b7713eb77ebf3ce6dd812c1b
yujinz <yujinzh18@gmail.com>
2017-10-09 22:00:30 -0500

    customize lifecycle

--- src/main/java/toy/plugin/GreetingMojo.java
+++ src/main/java/toy/plugin/GreetingMojo.java
@@ -2,6 +2,8 @@ package toy.plugin;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Execute;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
  
 /**
@@ -9,10 +11,11 @@ import org.apache.maven.plugins.annotations.Mojo;
  *
  */
 @Mojo( name = "sayhi")
+@Execute(phase = LifecyclePhase.TEST_COMPILE)
 public class GreetingMojo extends AbstractMojo
 {
     public void execute() throws MojoExecutionException
     {
-        getLog().info( "Hello, world." );
+        getLog().info( "Hello, world. " );
     }
 }
